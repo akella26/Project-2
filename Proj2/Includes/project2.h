4be283cf2ad50fd982c400fcdb2cd077e1eea05b
sreelab94 <sreela.bhogaraju@colorado.edu>
2017-07-19 19:09:41 -0600

    uart.h and project2.h

---
+++ Proj2/Includes/project2.h
@@ -0,0 +1,23 @@
+/*@file -project2.h
+ *@brief - header file for project2.c
+ *@author : Sreela , Sowmya
+ *@date : Jul 19 , 2017
+ */
+
+
+#ifndef __PROJECT2_H_
+#define __PROJECT2_H_
+#include<stdio.h>
+#include<stdint.h>
+
+/*@brief : calls the second project
+ *@ param : no
+ *@ return : no
+ */
+void project2();
+
+
+
+
+
+#endif /* __PROJECT2_H_ */
